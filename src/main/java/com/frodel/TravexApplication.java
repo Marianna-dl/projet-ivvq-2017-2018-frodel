09c313a4d63b22fcbc5c3c6c3d746539954f0b9a
Marianna-dl <marianna95220@gmail.com>
2018-02-22 11:15:24 +0100

    Initialisation du projet

---
+++ src/main/java/com/frodel/TravexApplication.java
@@ -0,0 +1,12 @@
+package com.frodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravexApplication.class, args);
+	}
+}
---
+++ src/test/java/com/frodel/TravexApplicationTests.java
@@ -0,0 +1,16 @@
+package com.frodel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravexApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
