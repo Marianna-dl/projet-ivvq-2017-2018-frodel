3ac59ce59495a730044605788c0399a18f57a92f
Marianna-dl <marianna95220@gmail.com>
2018-03-09 11:54:24 +0100

    Ajout dependance spock

--- src/test/java/com/frodel/TravexApplicationTests.java
+++ src/test/java/com/frodel/TravexApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import spock.lang.Specification;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TravexApplicationTests {
+public class TravexApplicationTests extends Specification {
 
 	@Test
 	public void contextLoads() {
