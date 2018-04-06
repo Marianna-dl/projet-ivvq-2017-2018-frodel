37b05634e151397eb6961b9f87d7cfb8b62ffe05
Marianna-dl <marianna95220@gmail.com>
2018-04-06 15:43:18 +0200

    Création du voyage #15 Test d'un voyage invalide

--- src/main/java/com/frodel/model/Travel.java
+++ src/main/java/com/frodel/model/Travel.java
@@ -1,10 +1,15 @@
 package com.frodel.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * This class represents one travel
  */
 public class Travel {
 
+    @NotNull
+    @Size(min = 3)
     private String name;
 
     /**
