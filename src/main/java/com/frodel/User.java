d42b3b1e6fbccf82d222c08299862440ebcb798c
franckpicardat <franck-ten@hotmail.fr>
2018-04-13 10:35:29 +0200

    tests Users

--- src/main/java/com/frodel/User.java
+++ src/main/java/com/frodel/User.java
@@ -1,9 +1,24 @@
-package com.frodel;/**
+package com.frodel;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
  * Created by hichem on 30/03/2018.
  */
 public class User {
+    @NotEmpty
     private String pseudo;
+    @NotNull
+    @NotEmpty
+    @Size (min=8)
     private String mdp;
+    @NotNull
+    @NotEmpty
+    @Email
     private String email;
     User(String pseudo, String mdp, String email) {
         this.pseudo = pseudo;
