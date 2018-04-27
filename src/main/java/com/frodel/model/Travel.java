60c36efaed4847e16fc3fa2ac856e18e38e9a626
Marianna-dl <marianna95220@gmail.com>
2018-04-27 09:35:03 +0200

    Insertion d'un voyage #18 Test d'intégration pour la sauvegarde d'un voyage avec le repository

--- src/main/java/com/frodel/model/Travel.java
+++ src/main/java/com/frodel/model/Travel.java
@@ -1,16 +1,25 @@
 package com.frodel.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 /**
  * This class represents one travel
  */
+@Entity
 public class Travel {
 
     @NotNull
     @Size(min = 3)
     private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     /**
      * Set a name of travel
@@ -20,4 +29,6 @@ public class Travel {
     {
         this.name = name;
     }
+
+
 }
--- src/main/java/com/frodel/services/TravelService.java
+++ src/main/java/com/frodel/services/TravelService.java
@@ -2,13 +2,16 @@ package com.frodel.services;
 
 import com.frodel.model.Travel;
 import com.frodel.repositories.TravelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Marianna on 13/04/2018.
  */
+@Service
 public class TravelService {
 
-
+    @Autowired
     private TravelRepository travelRepository;
 
     /**
