e298fbb19739ff4c381bdeecf305c25fcaa56662
Marianna-dl <marianna95220@gmail.com>
2018-05-09 15:27:30 +0200

    Insertion des lieux en bdd #16 Insertion d'une ville dans la bdd (tests du service unitaire + intégration)

--- src/main/java/com/frodel/model/City.java
+++ src/main/java/com/frodel/model/City.java
@@ -1,10 +1,20 @@
 package com.frodel.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-class City {
+@Entity
+public class City {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @NotNull
     @Size(min = 2)
