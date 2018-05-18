994c4da307c67f42b0c86b1aa9144737f89aeb30
LeaFoissac <lea.foissac@gmail.com>
2018-05-18 17:47:10 +0200

    API Doc #27

--- src/main/java/com/frodel/controller/UserController.java
+++ src/main/java/com/frodel/controller/UserController.java
@@ -13,6 +13,18 @@ public class UserController {
     @Autowired
     private UserRepository utilisateurRepository;
 
+    /**
+     * @api {post} /inscriptions/
+     * @apiName addUser
+     * @apiGroup User
+     * @apiDescription Add an user
+     *
+     * @apiParam {String} pseudo The pseudo of new user
+     * @apiParam {String} mdp The password of new user
+     * @apiParam {String} email The email of new user
+     *
+     * @apiSuccess {User} utilisateur The new user
+     */
     @RequestMapping(value = "/inscriptions", method = RequestMethod.POST)
     public User addUser(@RequestParam(value = "pseudo") String pseudo, @RequestParam(value = "mdp") String mdp, @RequestParam(value = "email") String email) {
         User utilisateur = new User(pseudo,mdp,email);
@@ -20,6 +32,15 @@ public class UserController {
         return utilisateur;
     }
 
+    /**
+     * @api {delete} /inscriptions/:userId
+     * @apiName deleteUser
+     * @apiGroup User
+     * @apiDescription Delete an user
+     *
+     * @apiParam {Long} userId The id of user to delete
+     *
+     */
     @RequestMapping(value = "/inscriptions/{user_id}", method = RequestMethod.DELETE)
     public void deleteUser(@PathVariable("user_id") Long userId) {
         utilisateurRepository.delete(userId);
