55195d300bb27b74eee5bec2edb3161acb162ae9
Marianna-dl <marianna95220@gmail.com>
2018-05-25 19:12:20 +0200

    Insertion des lieux en bdd #16 Test unitaire du service pour une place et code de la classe associée

---
+++ src/main/java/com/frodel/services/PlaceService.java
@@ -0,0 +1,21 @@
+package com.frodel.services;
+
+import com.frodel.model.Place;
+import com.frodel.repositories.PlaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PlaceService {
+
+    @Autowired
+    private PlaceRepository placeRepository;
+
+    /**
+     * Save the given place
+     *
+     * @param place the place
+     * @return the saved place
+     */
+    public Place savePlace(Place place) {
+        return placeRepository.save(place);
+    }
+}
